631f97bd95032e2b27a2ce22f65fb0103486fa8d
Kyle Kelly <kylejkelly@gmail.com>
2026-06-15 17:01:03 -0400

    f

--- src/infrastructure/crawler_factory.py
+++ src/infrastructure/crawler_factory.py
@@ -3,7 +3,15 @@ from crawlee.crawlers import PlaywrightCrawler, HttpCrawler
 class CrawlerFactory:
     @staticmethod
     def create_playwright_crawler(max_requests: int) -> PlaywrightCrawler:
-        return PlaywrightCrawler(max_requests_per_crawl=max_requests)
+        return PlaywrightCrawler(
+            max_requests_per_crawl=max_requests,
+            browser_type='chromium',
+            browser_options={
+                'chromium_sandbox': False,
+                'headless': True,
+                'args': ['--no-sandbox', '--disable-setuid-sandbox']
+            }
+        )
 
     @staticmethod
     def create_http_crawler(max_requests: int) -> HttpCrawler:
